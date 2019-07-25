cf762a0d1f7f42c8c4745d4a67009777cf3cc815
Bri <breezy.aguilar@gmail.com>
2019-07-24 23:06:40 -0500

    app is working perfectly

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        name: "Maria",
+        photo: "https://pbs.twimg.com/media/DzY4T15UwAACLQd?format=jpg&name=4096x4096",
+        scores: [
+            "5",
+            "3",
+            "3",
+            "2",
+            "2",
+            "1",
+            "5",
+            "2",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Haley",
+        photo: "https://www.google.com/search?q=haley+byers&rlz=1C1CHBF_enUS849US849&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiryYKRlM_jAhVROKwKHY7tDs0Q_AUIESgB&biw=1132&bih=789#imgrc=6F7tjje_ISESTM:",
+        scores: [
+            "3",
+            "1",
+            "4",
+            "2",
+            "5",
+            "1",
+            "2",
+            "4",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Lex",
+        photo: "https://www.google.com/search?q=alexis+curtiss&rlz=1C1CHBF_enUS849US849&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiXobWvlM_jAhUO0KwKHVb7AagQ_AUIESgB&biw=1132&bih=789#imgrc=zqhxbLXlWKdThM:",
+        scores: [
+            "2",
+            "4",
+            "1",
+            "4",
+            "5",
+            "2",
+            "3",
+            "1",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Bailey",
+        photo: "https://c8.alamy.com/comp/P48906/us-navy-boatswains-mate-seaman-apprentice-bailey-smith-assigned-to-amphibious-construction-battalion-1-prepares-a-us-navy-improved-navy-lighterage-system-causeway-ferry-for-on-loading-during-joint-logistics-over-the-shore-2016-jlots-16-june-13-2016-on-naval-magazine-indian-island-wash-jlots-16-is-a-joint-service-scenario-based-exercise-designed-to-simulate-disaster-and-humanitarian-assistance-in-the-cascadia-subduction-zone-us-air-force-photo-by-staff-sgt-kenneth-w-norman-P48906.jpg",
+        scores: [
+            "2",
+            "4",
+            "2",
+            "4",
+            "1",
+            "2",
+            "1",
+            "4",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "MacKenzie",
+        photo: "https://scontent-dfw5-1.cdninstagram.com/vp/64f741c2650aa8089e8ff529248489fe/5D8BB8E8/t51.2885-15/e35/s320x320/60363435_414347679171458_484519728266837359_n.jpg?_nc_ht=scontent-dfw5-1.cdninstagram.com&ig_cache_key=MjA1NDc3NzI5MDM0NTQ2MzMyMw%3D%3D.2",
+        scores: [
+            "1",
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Karly",
+        photo: "http://www.ubbulls.com/sports/sball/2011-12/photos/nevez-2012SB-0032.jpg?max_width=160&max_height=210",
+        scores: [
+            "3",
+            "4",
+            "1",
+            "4",
+            "4",
+            "2",
+            "1",
+            "2",
+            "4",
+            "1"
+        ]
+    }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require("express");
-var path = require("path");
 var app = express();
+var bodyParser = require("body-parser");
+
 var PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(bodyParser.json({type: "application/*+json"}));
 
+app.use(bodyParser.raw({type: "application/vnd.custon-type"}));
+
+app.use(bodyParser.text({type: "text/html"}));
+
+require("./app/routing/html-route.js")(app);
+
+require("./app/routing/api-route.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
